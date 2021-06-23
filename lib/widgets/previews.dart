578f8d10e65b895804c7751a87089a8d6e07e360
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-23 01:55:18 -0400

    Update previews.dart

--- lib/widgets/previews.dart
+++ lib/widgets/previews.dart
@@ -36,6 +36,7 @@ class Previews extends StatelessWidget {
               return GestureDetector(
                 onTap: () => print(content.name),
                 child: Stack(
+                  alignment: Alignment.center,
                   children: [
                     Container(
                       margin: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -49,6 +50,37 @@ class Previews extends StatelessWidget {
                         border: Border.all(color: content.color, width: 4.0),
                       ),
                     ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                      height: 130.0,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                            colors: [
+                              Colors.black87,
+                              Colors.black45,
+                              Colors.transparent
+                            ],
+                            stops: [
+                              0,
+                              0.5,
+                              1
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter),
+                        shape: BoxShape.circle,
+                        border: Border.all(color: content.color, width: 4.0),
+                      ),
+                    ),
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: SizedBox(
+                        height: 60.0,
+                        child: Image.asset(content.titleImageUrl),
+                      ),
+                    )
                   ],
                 ),
               );
