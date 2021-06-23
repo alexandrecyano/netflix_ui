ccaaecccd93a603e8e9dc9044c6e13f4a3079328
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-23 00:17:22 -0400

    previews

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -51,6 +51,12 @@ class _HomeScreenState extends State<HomeScreen> {
         slivers: <Widget>[
           SliverToBoxAdapter(
             child: ContentHeader(featuredContent: sintelContent),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: Previews(title: 'Previews', contentList: previews),
+            ),
           )
         ],
       ),
---
+++ lib/widgets/previews.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_ui/models/content_model.dart';
+
+class Previews extends StatelessWidget {
+  final String title;
+  final List<Content> contentList;
+
+  const Previews({Key? key, this.title = '', required this.contentList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          height: 165.0,
+          child: ListView.builder(
+            padding:
+                const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+            scrollDirection: Axis.horizontal,
+            itemCount: contentList.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Content content = contentList[index];
+              return GestureDetector(
+                onTap: () => print(content.name),
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                      height: 130.0,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(content.imageUrl),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
