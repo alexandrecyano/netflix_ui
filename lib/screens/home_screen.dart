6d66c70a9cdd69810245e498095a2fcec5f28a1c
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-25 02:05:27 -0400

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -55,17 +55,22 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverPadding(
             padding: const EdgeInsets.only(top: 20.0),
             sliver: SliverToBoxAdapter(
-              child: Previews(title: 'Previews', contentList: previews),
+              child: Previews(
+                  key: PageStorageKey('previews'),
+                  title: 'Previews',
+                  contentList: previews),
             ),
           ),
           SliverToBoxAdapter(
             child: ContentList(
+              key: PageStorageKey('myList'),
               title: 'My List',
               contentList: myList,
             ),
           ),
           SliverToBoxAdapter(
             child: ContentList(
+              key: PageStorageKey('originals'),
               title: 'Netflix Originals',
               contentList: originals,
               isOriginals: true,
@@ -75,6 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.only(bottom: 20.0),
             sliver: SliverToBoxAdapter(
               child: ContentList(
+                key: PageStorageKey('trending'),
                 title: 'Trending',
                 contentList: trending,
               ),
